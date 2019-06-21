841a11e81d71269a0d62c61c5b7387934df4c0a6
Egor Radchenko <egor.radchenko@quantumsoft.ru>
2019-06-21 13:00:41 +0700

    integration test: added _STAGE substitution

--- integration_test/run_adapter_and_wait.sh
+++ integration_test/run_adapter_and_wait.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# project location dataset store adapter_port finish_port
-gradle run -Dexec.args='--dimse_aet=IMPORTADAPTER --dimse_port='$5' --dicomweb_addr=https://healthcare.googleapis.com/v1beta1/projects/'$1'/locations/'$2'/datasets/'$3'/dicomStores/'$4'/dicomWeb --dicomweb_stow_path=/studies --dimse_cmove_aet=CMOVESUB --verbose' &
+# apiStage project location dataset store adapter_port finish_port
+gradle run -Dexec.args='--dimse_aet=IMPORTADAPTER --dimse_port='$6' --dicomweb_addr=https://healthcare.googleapis.com/'$1'/projects/'$2'/locations/'$3'/datasets/'$4'/dicomStores/'$5'/dicomWeb --dicomweb_stow_path=/studies --dimse_cmove_aet=CMOVESUB --verbose' &
 apt-get update
 apt-get install -y netcat-openbsd
-echo 'Waiting for connection on port '$6' to finish step'
-nc -l -p $6
+echo 'Waiting for connection on port '$7' to finish step'
+nc -l -p $7
 echo 'Finishing adapter run'
