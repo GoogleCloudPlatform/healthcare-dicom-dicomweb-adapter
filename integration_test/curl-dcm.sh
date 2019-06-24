3ceefe176c5dddd76e3a6d08283ccadee6f54f7f
Egor Radchenko <egor.radchenko@quantumsoft.ru>
2019-06-24 11:59:43 +0700

    integration test: minor changes - _STAGE renamed to _VERSION - added Accept header to curl to avoid having to handle multipart

--- integration_test/curl-dcm.sh
+++ integration_test/curl-dcm.sh
@@ -2,5 +2,5 @@
 # url, output_file
 curl -X GET \
      -H "Authorization: Bearer "$(gcloud auth print-access-token) \
-     $1 \
-     |tail -c+137 | head -c-68 >$2
+     -H "Accept: application/dicom; transfer-syntax=*" \
+     $1 --output $2
