a9e6c2a3d6763ace52c3f45fb86cdf4f252e6cd2
Egor Radchenko <egor.radchenko@quantumsoft.ru>
2019-07-04 14:35:41 +0700

    restored c-find monitoring events

--- import/src/main/java/com/google/cloud/healthcare/imaging/dicomadapter/monitoring/Event.java
+++ import/src/main/java/com/google/cloud/healthcare/imaging/dicomadapter/monitoring/Event.java
@@ -5,7 +5,13 @@ public enum Event implements IMonitoringEvent {
 
   CSTORE_REQUEST(Constants.prefix + "cstore_requests"),
   CSTORE_ERROR(Constants.prefix + "cstore_errors"),
-  CSTORE_BYTES(Constants.prefix + "cstore_bytes");
+  CSTORE_BYTES(Constants.prefix + "cstore_bytes"),
+
+  CFIND_REQUEST(Constants.prefix + "cfind_requests"),
+  CFIND_ERROR(Constants.prefix + "cfind_errors"),
+  CFIND_CANCEL(Constants.prefix + "cfind_cancels"),
+  CFIND_SUB_QIDORS_REQUEST(Constants.prefix + "cfind_sub_qidors_requests"),
+  CFIND_SUB_QIDORS_ERROR(Constants.prefix + "cfind_sub_qidors_errors");
 
   private final String metricName;
 
