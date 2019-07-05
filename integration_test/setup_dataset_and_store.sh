365570888c4b6425e02d818b0279cbe916eb4f10
Egor Radchenko <egor.radchenko@quantumsoft.ru>
2019-07-05 12:23:54 +0700

    integration test changes: - can use dicom dataset/store in different project - defaults set to gcp-healthcare-oss-test/us-central1/healthcare-dicom-dicomweb-adapter-test

--- integration_test/setup_dataset_and_store.sh
+++ integration_test/setup_dataset_and_store.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-# location dataset store_name
-gcloud alpha healthcare datasets describe $2 "--location="$1 "--quiet" >/dev/null
+# project location dataset store_name
+gcloud alpha healthcare datasets describe $3 "--project="$1 "--location="$2 "--quiet" >/dev/null
 if [ $? -ne 0 ];
 then
-  gcloud alpha healthcare datasets create $2 "--location="$1 "--quiet" || exit $?
+  gcloud alpha healthcare datasets create $3 "--project="$1 "--location="$2 "--quiet" || exit $?
 fi
 
-gcloud alpha healthcare dicom-stores describe $3 "--dataset="$2 "--location="$1 "--quiet" >/dev/null
+gcloud alpha healthcare dicom-stores describe $4 "--project="$1 "--location="$2 "--dataset="$3 \
+ "--quiet" >/dev/null
 if [ $? -eq 0 ];
 then
-  gcloud alpha healthcare dicom-stores delete $3 "--dataset="$2 "--location="$1 "--quiet" || exit $?
+  gcloud alpha healthcare dicom-stores delete $4 "--project="$1 "--location="$2 "--dataset="$3 \
+   "--quiet" || exit $?
 fi
-gcloud alpha healthcare dicom-stores create $3 "--dataset="$2 "--location="$1 "--quiet"
+gcloud alpha healthcare dicom-stores create $4 "--project="$1 "--location="$2 "--dataset="$3 \
+ "--quiet"
 
