7298a675cdc451819422001d310e9749d59345db
Egor Radchenko <egor.radchenko@quantumsoft.ru>
2019-08-02 17:27:34 +0700

    [#2] cleanup - same naming scheme for sh files

--- integration_test/run-adapter-and-wait.sh
+++ integration_test/run-adapter-and-wait.sh
@@ -3,6 +3,6 @@
 gradle run -Dexec.args='--dimse_aet=IMPORTADAPTER --dimse_port='$6' --dicomweb_addr=https://healthcare.googleapis.com/'$1'/projects/'$2'/locations/'$3'/datasets/'$4'/dicomStores/'$5'/dicomWeb --dicomweb_stow_path=/studies --aet_dictionary_inline=[{"name":"STORESCP","host":'$8',"port":'$9'}] --verbose' &
 apt-get update
 apt-get install -y netcat-openbsd
-echo 'Waiting for connection on port '$7' to finish adapter-run step'
+echo 'Waiting for connection on port '$7' to finish adapter-main step'
 nc -l -p $7
-echo 'Finishing adapter-run step'
+echo 'Finishing adapter-main step'
