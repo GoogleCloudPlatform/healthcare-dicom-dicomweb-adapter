5aa643cf0b5e762598801036812436ebb99c9e36
Egor Radchenko <egor.radchenko@quantumsoft.ru>
2019-08-07 12:02:32 +0700

    [#2] review changes

--- integration_test/run-adapter-and-wait.sh
+++ integration_test/run-adapter-and-wait.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# apiStage project location dataset store adapter_port finish_port storescp_host store_scp_port
+# apiStage project location dataset store adapter_port finish_port storescp_host store_scp_port commitmentscu_host commitmentscu_port
 gradle run -Dexec.args='--dimse_aet=IMPORTADAPTER --dimse_port='$6' --dicomweb_addr=https://healthcare.googleapis.com/'$1'/projects/'$2'/locations/'$3'/datasets/'$4'/dicomStores/'$5'/dicomWeb --dicomweb_stow_path=/studies --aet_dictionary_inline=[{"name":"STORESCP","host":'$8',"port":'$9'},{"name":"STGCMTSCU","host":'${10}',"port":'${11}'},] --verbose' &
 apt-get update
 apt-get install -y netcat-openbsd
