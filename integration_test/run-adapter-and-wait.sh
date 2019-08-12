d346b4b272a214d8745130aee6664a93ddc87438
Egor Radchenko <egor.radchenko@quantumsoft.ru>
2019-08-12 12:29:05 +0700

    [#19] merge changes

--- integration_test/run-adapter-and-wait.sh
+++ integration_test/run-adapter-and-wait.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # apiStage project location dataset store adapter_port finish_port storescp_host store_scp_port commitmentscu_host commitmentscu_port
-gradle run -w -Dexec.args='--dimse_aet=IMPORTADAPTER --dimse_port='$6' --dicomweb_addr=https://healthcare.googleapis.com/'$1'/projects/'$2'/locations/'$3'/datasets/'$4'/dicomStores/'$5'/dicomWeb --dicomweb_stow_path=/studies --aet_dictionary_inline=[{"name":"STORESCP","host":'$8',"port":'$9'},{"name":"STGCMTSCU","host":'${10}',"port":'${11}'},] --verbose' &
+gradle run -w -Dexec.args='--dimse_aet=IMPORTADAPTER --dimse_port='$6' --dicomweb_address=https://healthcare.googleapis.com/'$1'/projects/'$2'/locations/'$3'/datasets/'$4'/dicomStores/'$5'/dicomWeb --aet_dictionary_inline=[{"name":"STORESCP","host":'$8',"port":'$9'},{"name":"STGCMTSCU","host":'${10}',"port":'${11}'},] --verbose' &
 apt-get -qq update
 apt-get -qq install -y netcat-openbsd
 echo 'Waiting for connection on port '$7' to finish adapter-main step'
