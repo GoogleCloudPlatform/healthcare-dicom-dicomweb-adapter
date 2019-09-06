37fa2acb3152510910a741a9a94b3aa9b989090e
Egor Lezhnin <egor.lezhnin@quantumsoft.ru>
2019-09-07 06:47:51 +0700

    [#13] versioning for images jdk11

--- integration_test/build-adapters.sh
+++ integration_test/build-adapters.sh
@@ -12,10 +12,14 @@ build_adapter () {
   gradle dockerBuildImage -Pdocker_tag=${adapter_name}:$version.$postfix
   gradle dockerBuildImage -Pdocker_tag=${adapter_name}:$2
 }
-
+apt-get -qq update
+apt-get install -y gnupg2
+# we need gcloud and 5.6-jdk11 in same image next 4 lines adds google's package storage and installs gcloud by this manual https://cloud.google.com/sdk/docs/quickstart-debian-ubuntu
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 apt-get -qq update
 apt-get install google-cloud-sdk -y
-apt-get -qq install -y gradle
+
 gradle build
 cd ./import
 build_adapter import $2
