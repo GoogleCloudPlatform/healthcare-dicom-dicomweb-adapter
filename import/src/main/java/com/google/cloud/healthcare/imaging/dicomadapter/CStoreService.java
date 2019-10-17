a8506f4a72db0b5129e913d8ccff78c42261148e
Egor Radchenko <egor.radchenko@quantumsoft.ru>
2019-10-17 16:03:44 +0700

    [#22] back to basic DicomRedactor (since streaming changes eventually didn't work out)

--- import/src/main/java/com/google/cloud/healthcare/imaging/dicomadapter/CStoreService.java
+++ import/src/main/java/com/google/cloud/healthcare/imaging/dicomadapter/CStoreService.java
@@ -16,7 +16,7 @@ package com.google.cloud.healthcare.imaging.dicomadapter;
 
 import com.google.cloud.healthcare.IDicomWebClient;
 import com.google.cloud.healthcare.IDicomWebClient.DicomWebException;
-import com.google.cloud.healthcare.deid.redactor.IDicomRedactor;
+import com.google.cloud.healthcare.deid.redactor.DicomRedactor;
 import com.google.cloud.healthcare.imaging.dicomadapter.monitoring.Event;
 import com.google.cloud.healthcare.imaging.dicomadapter.monitoring.MonitoringService;
 import com.google.common.io.CountingInputStream;
@@ -46,9 +46,9 @@ public class CStoreService extends BasicCStoreSCP {
 
   private final String path;
   private final IDicomWebClient dicomWebClient;
-  private final IDicomRedactor redactor;
+  private final DicomRedactor redactor;
 
-  CStoreService(String path, IDicomWebClient dicomWebClient, IDicomRedactor redactor) {
+  CStoreService(String path, IDicomWebClient dicomWebClient, DicomRedactor redactor) {
     this.path = path;
     this.dicomWebClient = dicomWebClient;
     this.redactor = redactor;
