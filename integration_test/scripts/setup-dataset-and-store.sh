e8fa4965905e0cd2315a0064616cbfd857333798
Egor Radchenko <egor.radchenko@quantumsoft.ru>
2019-11-28 14:32:38 +0700

    [#52] switch integration tests to use "gcloud beta"

--- integration_test/scripts/setup-dataset-and-store.sh
+++ integration_test/scripts/setup-dataset-and-store.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 # project location dataset store_name
-gcloud alpha healthcare datasets describe $3 "--project="$1 "--location="$2 "--quiet" >/dev/null
+gcloud beta healthcare datasets describe $3 "--project="$1 "--location="$2 "--quiet" >/dev/null
 if [ $? -ne 0 ];
 then
-  gcloud alpha healthcare datasets create $3 "--project="$1 "--location="$2 "--quiet" || exit $?
+  gcloud beta healthcare datasets create $3 "--project="$1 "--location="$2 "--quiet" || exit $?
 fi
 
-gcloud alpha healthcare dicom-stores describe $4 "--project="$1 "--location="$2 "--dataset="$3 \
+gcloud beta healthcare dicom-stores describe $4 "--project="$1 "--location="$2 "--dataset="$3 \
  "--quiet" >/dev/null
 if [ $? -eq 0 ];
 then
-  gcloud alpha healthcare dicom-stores delete $4 "--project="$1 "--location="$2 "--dataset="$3 \
+  gcloud beta healthcare dicom-stores delete $4 "--project="$1 "--location="$2 "--dataset="$3 \
    "--quiet" || exit $?
 fi
-gcloud alpha healthcare dicom-stores create $4 "--project="$1 "--location="$2 "--dataset="$3 \
+gcloud beta healthcare dicom-stores create $4 "--project="$1 "--location="$2 "--dataset="$3 \
  "--quiet"
 
