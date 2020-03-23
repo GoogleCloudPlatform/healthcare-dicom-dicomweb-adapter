9fe84b01a3f831260372ef36caed31875111017a
Egor Radchenko <egor.radchenko@quantumsoft.ru>
2020-03-23 14:20:28 +0700

    [#56] review: build jai-imageio-jpeg2000 as gradle sub-project
    - System.runFinalizersOnExit(true) is commented out because jdk above 8 doesn't support that
    (was used for performance profiling build only and off by default).

--- third_party/jai-imageio-jpeg2000/src/main/java/jj2000/j2k/entropy/decoder/StdEntropyDecoder.java
+++ third_party/jai-imageio-jpeg2000/src/main/java/jj2000/j2k/entropy/decoder/StdEntropyDecoder.java
@@ -622,7 +622,7 @@ public class StdEntropyDecoder extends EntropyDecoder
         if (DO_TIMING) {
             time = new long[src.getNumComps()];
             // If we are timing make sure that 'finalize' gets called.
-            System.runFinalizersOnExit(true);
+            //System.runFinalizersOnExit(true);
         }
 
         // Initialize internal variables
