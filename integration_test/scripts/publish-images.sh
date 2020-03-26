954a65fd1200bd2337f54686e7b4d4be7b7659f8
Egor Lezhnin <egor.lezhnin@quantumsoft.ru>
2020-03-26 20:04:59 +0700

    [#60] gitHub releases

--- integration_test/scripts/publish-images.sh
+++ integration_test/scripts/publish-images.sh
@@ -1,14 +1,43 @@
 #!/bin/bash
-# imageProject publishFlag
+# imageProject publishFlag repositoryString
+
+accessToken=$ACCESS_TOKEN
+repositoryString=$3
+
+# repositoryString contains "githubUser_githubRepo" if repository is stored by user 
+# and just "githubRepo" if stored by organization https://cloud.google.com/cloud-build/docs/configuring-builds/substitute-variable-values
+IFS='_' read -r -a repoArray <<< "$repositoryString"
+githubUser="${repoArray[1]}"
+githubRepo="${repoArray[2]}"
+if [ -z "$githubUser" ]
+then
+    githubUser="GoogleCloudPlatform"
+    githubRepo=$3
+fi
 
 base_name="gcr.io/${1}/healthcare-api-dicom-dicomweb-adapter-"
 
 publish_adapter () {
   adapter_name=${base_name}${1}
-  docker push $adapter_name
+  docker push $adapter_name > /dev/null 2> /dev/null
+  echo "https://${adapter_name}:${2}"
 }
 if [ "$2" == "true" ]
 then
-  publish_adapter import
-  publish_adapter export
+  # getting local tag version by filtering adapter image from list of images, getting tags
+  # and filtering version tag by `grep "\."` 
+  # have to do this because docker does not allow to search through tags in local storage
+  version=`docker images | grep $base_name | awk '{print $2}' | grep "\." | head -n 1`
+  version=${version[0]}
+  import=$(publish_adapter import $version)
+  export=$(publish_adapter export $version)
+  body="$import\n$export"
+  echo {\"tag_name\": \"$version\",\"name\": \"$version\",\"body\": \"$body\"} > /workspace/request.json
+  responseCode=$(curl -# -XPOST -H 'Content-Type:application/json' -H 'Accept:application/json' -w "%{http_code}" --data-binary @/workspace/request.json \
+  https://api.github.com/repos/$githubUser/$githubRepo/releases?access_token=$accessToken -o response.json)  
+  if [ $responseCode -ne 201   ]; then
+    cat response.json
+    exit 1;
+  fi
+
 fi
