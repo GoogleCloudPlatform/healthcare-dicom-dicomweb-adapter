00e8addb1e0d2e5e65f59339fcae57ed9b21ada3
Egor Lezhnin <egor.lezhnin@quantumsoft.ru>
2020-04-01 21:45:48 +0700

    [#60] refactor of release

--- integration_test/scripts/publish-images.sh
+++ integration_test/scripts/publish-images.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-# imageProject publishFlag repositoryString
+# imageProject publishFlag repositoryString ACCESS_TOKEN_BASE64 PROJECT_ID KMS_LOCATION KMS_KEYRING KMS_KEY
 
-accessToken=$ACCESS_TOKEN
 repositoryString=$3
+ACCESS_TOKEN_BASE64=$4
+PROJECT_ID=$5
+KMS_LOCATION=$6
+KMS_KEYRING=$7
+KMS_KEY=$8
+
+# decryption of github access token
+accessToken=$(echo -n $ACCESS_TOKEN_BASE64 | base64 -d - | gcloud kms decrypt --ciphertext-file - --plaintext-file - --project $PROJECT_ID --location $KMS_LOCATION --keyring $KMS_KEYRING --key $KMS_KEY)
 
 # repositoryString contains "githubUser_githubRepo" if repository is stored by user 
 # and just "githubRepo" if stored by organization https://cloud.google.com/cloud-build/docs/configuring-builds/substitute-variable-values
@@ -32,12 +39,16 @@ then
   import=$(publish_adapter import $version)
   export=$(publish_adapter export $version)
   body="$import\n$export"
+  echo $version
+  echo $githubUser
+  echo $githubRepo
   echo {\"tag_name\": \"$version\",\"name\": \"$version\",\"body\": \"$body\"} > /workspace/request.json
   responseCode=$(curl -# -XPOST -H 'Content-Type:application/json' -H 'Accept:application/json' -w "%{http_code}" --data-binary @/workspace/request.json \
   https://api.github.com/repos/$githubUser/$githubRepo/releases?access_token=$accessToken -o response.json)  
-  if [ $responseCode -ne 201   ]; then
-    cat response.json
+  cat response.json  
+  if [ $responseCode -ne 201   ]; then  
     exit 1;
   fi
+
 
 fi
