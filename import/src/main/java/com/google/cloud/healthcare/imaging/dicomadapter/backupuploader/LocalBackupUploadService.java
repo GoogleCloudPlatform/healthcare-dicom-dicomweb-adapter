6969c69288b30025900248ef784ae1d07c18b1dd
oleksii opanasenko <oleksii.opanasenko@zazmic.com>
2020-07-20 17:49:31 +0400

    local uploader impl added;

--- import/src/main/java/com/google/cloud/healthcare/imaging/dicomadapter/backupuploader/LocalBackupUploadService.java
+++ import/src/main/java/com/google/cloud/healthcare/imaging/dicomadapter/backupuploader/LocalBackupUploadService.java
@@ -1,6 +1,8 @@
 package com.google.cloud.healthcare.imaging.dicomadapter.backupuploader;
 
-import com.google.cloud.healthcare.IDicomWebClient;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class LocalBackupUploadService extends AbstractBackupUploadService {
     public LocalBackupUploadService(String uploadFilePath, int attemptsCount) {
@@ -8,14 +10,23 @@ public class LocalBackupUploadService extends AbstractBackupUploadService {
     }
 
     @Override
-    public void doWriteBackup(byte[] backupData, String uploadFilePath) {
-        // todo: implement_me
+    public void doWriteBackup(byte[] backupData, String uploadFilePath) throws BackupExeption {
+        try (FileOutputStream fos = new FileOutputStream(uploadFilePath)) {
+            fos.write(backupData, 0, backupData.length);
+        } catch (IOException ex){
+            throw new BackupExeption("Error with writing backup file", ex);
+        }
     }
 
     @Override
-    public byte[] doReadBackup(String uploadFilePath) {
-        return new byte[0];
-        // todo: implement_me
+    public byte[] doReadBackup(String uploadFilePath) throws BackupExeption {
+        try (FileInputStream fin = new FileInputStream(uploadFilePath)) {
+            byte[] buffer = new byte[fin.available()];
+            fin.read(buffer, 0, fin.available());
+            return buffer;
+        } catch (IOException ex) {
+            throw new BackupExeption("Error with reading backup file", ex);
+        }
     }
 
     @Override
